ace26ab937d640c12ef5ea1ffb37de1b19196f0c
Sergio Poveda <sf.poveda@uniandes.edu.co>
2026-07-20 17:44:48 -0500

    Added the first example about tensor models

---
+++ examples/00_tensor_models.py
@@ -0,0 +1,8 @@
+from discretize import TensorMesh
+from discretize.utils import active_from_xyz
+from simpeg.utils import mkvc, model_builder
+from simpeg import maps
+import numpy as np
+import matplotlib.pyplot as plt
+
+
